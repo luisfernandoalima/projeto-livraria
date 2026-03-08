f3227ecb858fb72660610acd249b0ffaafc8ab70
Luis Lima <luisfernando.lima2005@gmail.com>
2026-03-08 20:47:35 -0300

    feat/Criação da classe Produto

---
+++ server/api/src/class/Produto.ts
@@ -0,0 +1,21 @@
+export default class Produto {
+  private _id: number;
+  private _titulo: String;
+  private _subtitulo: String;
+  private _sinopse: String;
+  private _autor: String;
+  private _serie: String;
+  private _volume: String;
+  private _isbn10: String;
+  private _isbn13: String;
+  private _formato: String;
+  private _numPaginas: number;
+  private _idioma: String;
+  private _dataPublicacao: Date;
+  private _genero: String;
+  private _classIndicativa: String;
+  private _precoDouble: String;
+  private _estoque: String;
+  private _status: String;
+  private _imgCapa: String;
+}
