17a47d6b371015c733f61c86d22154f12331a0d0
Luis Lima <luisfernando.lima2005@gmail.com>
2026-03-08 19:40:33 -0300

    feat/Criação da classe Usuário

--- server/api/src/class/Usuario.ts
+++ server/api/src/class/Usuario.ts
@@ -1,24 +1,42 @@
-export default class User {
-  constructor() {
-    this.id = 1;
-    this.name = "Luis";
-    this.email = "luis@gmail.com";
-    this.cargo = "admin";
+export default class Usuario {
+  constructor(id: number, nome: String, email: String, cargo: String) {
+    this._id = id;
+    this._nome = nome;
+    this._email = email;
+    this._cargo = cargo;
   }
 
-  private id: number;
-  private name: String;
-  private email: String;
-  private cargo: String;
+  private _id: number;
+  private _nome: String;
+  private _email: String;
+  private _cargo: String;
+
+  getId = (): number => this._id;
+
+  setId = (id: number) => {
+    this._id = id;
+  };
+
+  getNome = (): String => this._nome;
+
+  setNome = (nome: String) => {
+    this._nome = nome;
+  };
+
+  getemail = (): String => this._email;
+
+  setEmail = (email: String) => {
+    this._email = email;
+  };
+
+  getCargo = (): String => this._cargo;
 
   Create = (): boolean => {
     return false;
   };
 
-  Read = (): User => {
-    let user = new User();
-
-    return user;
+  Read = (): boolean => {
+    return true;
   };
 
   Update = (): boolean => {
