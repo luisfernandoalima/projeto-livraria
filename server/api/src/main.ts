cede5f4f05e08549cd9ef91d6965c16c550356d5
Luis Lima <luisfernando.lima2005@gmail.com>
2026-03-08 16:33:45 -0300

    test/Preparando rotas da api

--- server/api/src/main.ts
+++ server/api/src/main.ts
@@ -1,10 +1,14 @@
-import express from 'express'
-import type { Application } from 'express';
-import cors from "cors"
+import express from "express";
+import type { Application } from "express";
+import cors from "cors";
 import "dotenv/config";
 
-const app:Application = express();
-app.use(express.json())
-app.use(cors())
+import route from "./routes/route.js";
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/", route);
 
 export default app;
