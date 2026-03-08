96fb5e7f4391dce6f7d294f4fc72214952f31415
Luis Lima <luisfernando.lima2005@gmail.com>
2026-03-08 20:51:34 -0300

    feat/Criação da classe Saída

---
+++ server/api/src/class/Saida.ts
@@ -0,0 +1,9 @@
+export default class Saida {
+  private _id: number;
+  private _precoTotal: number;
+  private cliente: String;
+  private _data: Date;
+  private _status: String;
+  private _cpfCliente: String;
+  private _tipoPagamento: String;
+}
