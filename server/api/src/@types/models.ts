6077a2c1aef28f4f5ce8853356b3bbef79c5e952
Luis Lima <luisfernando.lima2005@gmail.com>
2026-04-11 16:07:50 -0300

    feat/Criação da função calcularPrecoTotal

--- server/api/src/@types/models.ts
+++
@@ -1,8 +0,0 @@
-import Produto from "../class/Produto.js";
-
-export type TEntradaProduto = {
-  produto: Produto;
-  quantidade: number;
-};
-
-export type TSaidaProduto = TEntradaProduto;
--- server/api/src/utils/calcularPrecoTotal.ts
+++ server/api/src/utils/calcularPrecoTotal.ts
@@ -1,5 +1,12 @@
-const calcularPrecoTotal = () => {
-  const valor = 10;
+import Registro from "../class/Registro.js";
+
+const calcularPrecoTotal = (registro: Registro[]) => {
+  let valor = 0;
+
+  registro.forEach((produto) => {
+    valor += produto.getPrecoItens();
+  });
+
   return valor;
 };
 
