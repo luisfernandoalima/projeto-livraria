a4ca94faee27beb75bf6a7f94f9a1260b6b307f5
Luis Lima <luisfernando.lima2005@gmail.com>
2026-05-30 19:51:14 -0300

    feat/Barra de busca de colaboradores

--- app/desktop/app/pages/colaborador/index.vue
+++ app/desktop/app/pages/colaborador/index.vue
@@ -2,28 +2,68 @@
 import SearchBar from "~/components/layout/SearchBar.vue";
 import { Icon } from "@iconify/vue";
 
-import { useApi } from "#imports";
 definePageMeta({
   middleware: "auth",
   layout: "default",
 });
 
+const route = useRoute();
+const router = useRouter();
+
 const api = useApi();
 const token = useCookie("auth_token");
 
-const response = await api("/user/list-users", {
-  method: "GET",
-  headers: {
-    authorization: `Bearer ${token.value}`,
-  },
-});
+const colaborador = ref(route.query.nome ?? "");
 
-console.log(response);
+const colaboradores = ref([]);
+
+const buscarColaboradores = () => {
+  router.push(`/colaborador?nome=${colaborador.value}`);
+};
+
+const carregarColaboradores = async () => {
+  const nomeColaborador = colaborador.value;
+
+  let response;
+
+  if (!nomeColaborador) {
+    response = await api("/user/list-users", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token.value}`,
+      },
+    });
+  } else {
+    response = await api(`/user/list-by-name/${nomeColaborador}`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token.value}`,
+      },
+    });
+  }
+
+  colaboradores.value = response.users;
+
+  console.log(colaboradores.value);
+  console.log(colaboradores);
+};
+
+watch(
+  () => route.query.nome,
+  () => {
+    carregarColaboradores();
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
   <NuxtLayout>
-    <SearchBar />
+    <SearchBar
+      :handleForms="buscarColaboradores"
+      :value="colaborador"
+      @update:value="colaborador = $event"
+    />
 
     <div class="button_area">
       <NuxtLink to="/colaborador/novo-colaborador"
@@ -45,7 +85,7 @@ console.log(response);
           </tr>
         </thead>
         <tbody>
-          <tr v-for="value in response">
+          <tr v-for="value in colaboradores">
             <td class="border border-gray-300">{{ value.nome }}</td>
             <td class="border border-gray-300">{{ value.email }}</td>
             <td class="border border-gray-300">{{ value.telefone }}</td>
